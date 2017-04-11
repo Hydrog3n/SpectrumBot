1e32d6040059d2cc371d30b94ea66e35584243eb
Hydrog3n <loic.vaille@gmail.com>
2017-04-11 15:03:29 +0200

    /turn/:idjoueur implementé

--- routes/api.js
+++ routes/api.js
@@ -19,11 +19,13 @@ router.get('/connect/:groupname', function(req, res, next) {
         if (result.length > 0) {
             async.forEach(result, function(game, next) {
                 if (game.finpartie == false && game.player.length == 1) {
-                    models.collections.player.create({ "name": req.params.groupname}, function(err, model) {
+                    console.log("add player");
+                    models.collections.player.create({ "name": req.params.groupname, "numerojoueur": 2}, function(err, model) {
                         if (err) {res.status(503).json(err)}
                         player = {
                             "nomJoueur": req.params.groupname,
                             "idjoueur": model.id,
+                            "numerojoueur": model.numerojoueur,
                             "code": 200
                         };
                         game.player.push(model);
@@ -31,15 +33,18 @@ router.get('/connect/:groupname', function(req, res, next) {
                             res.status(200).json(player);
                         });
                     });
+                } else {
+                    next();
                 }
-                next()
             }, function(err) {
+                console.log('done');
                 if (err) return res.status(503).json(err);
-                models.collections.player.create({ "name": req.params.groupname}, function(err, model) {
+                models.collections.player.create({ "name": req.params.groupname, "numerojoueur": 1}, function(err, model) {
                     if (err) {res.status(503).json(err)}
                     player = {
                         "nomJoueur": req.params.groupname,
                         "idjoueur": model.id,
+                        "numerojoueur": model.numerojoueur,
                         "code": 200
                     };
                     var newgame = {
@@ -54,11 +59,13 @@ router.get('/connect/:groupname', function(req, res, next) {
                 });
             });
         } else {
-            models.collections.player.create({ "name": req.params.groupname}, function(err, model) {
+            console.log('done');
+            models.collections.player.create({ "name": req.params.groupname, "numerojoueur": 1}, function(err, model) {
                 if (err) {res.status(503).json(err)}
                 player = {
                     "nomJoueur": req.params.groupname,
                     "idjoueur": model.id,
+                    "numerojoueur": model.numerojoueur,
                     "code": 200
                 };
                 var newgame = {
@@ -76,11 +83,35 @@ router.get('/connect/:groupname', function(req, res, next) {
     });
 });
 
-router.post('/turn/:idplayer', function(req, res, next) {
-    models.collections.player.findOne({where : {id: req.params.id}}, function(err, result) {
-        console.log(result);
+router.get('/turn/:idplayer', function(req, res, next) {
+    //console.log(req.params.idplayer);
+    models.collections.game.find(function(err, games) {
+        async.each(games,function(game, next) {
+            console.log('game : '+game.id);
+            async.each(game.player, function(player, next) {
+                console.log('player : '+player.id);
+                if (player.id == req.params.idplayer) {
+                    var result = {
+                        "status": game.playerturn,
+                        "tableau": game.tableau,
+                        "nbTenaillesJ1": game.player[0].nbtennaille,
+                        "nbTenaillesJ2": game.player[1].nbtennaille,
+                        "dernierCoupX": 0, //TODO
+                        "dernierCoupY": 0, //TODO 
+                        "prolongation": game.prolongation,
+                        "finPartie": game.finpartie,
+                        "detailFinPartie": game.detailfinpartie,
+                        "numTour": game.numtour,
+                        "code": 200
+                    }
+                    res.status(200).json(result);
+                } else {
+                    next();
+                }
+            }, next);
+        }, function() {
+            res.sendStatus(404);
+        });
     });
-
-
 });
 module.exports = router;
