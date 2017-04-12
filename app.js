8b47365f42971a3a735c2e29e4122a5c5baaabea
Hydrog3n <loic.vaille@gmail.com>
2017-04-12 11:04:39 +0200

    add CORS

--- app.js
+++ app.js
@@ -14,6 +14,19 @@ models.initialize(connections, function(err, models) {
     app.use("/", routes);
 
     app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Methods", "POST, PUT, GET, DELETE, OPTIONS");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-lambda-key");
+
+        if (req.method === "OPTIONS") {
+            res.status(200);
+            return res.send();
+        }
+
+        next();
+    });
+
+    app.use(function(req, res, next) {
         res.status(404);
         res.send({
             "data":{
